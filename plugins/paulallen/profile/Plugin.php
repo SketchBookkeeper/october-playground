f3de6b273afa037cdf61d8751712683e7a7a544c
Paul Allen <p.allen.art@gmail.com>
2019-03-08 10:38:01 -0700

    Make bio fillable.

--- plugins/paulallen/profile/Plugin.php
+++ plugins/paulallen/profile/Plugin.php
@@ -2,6 +2,7 @@
 
 use System\Classes\PluginBase;
 use RainLab\User\Controllers\Users as UsersController;
+use RainLab\User\Models\User as UserModel;
 
 class Plugin extends PluginBase
 {
@@ -15,6 +16,13 @@ class Plugin extends PluginBase
 
     public function boot()
     {
+        // Extend user model to have 'bio' as a fillable field.
+        UserModel::extend(function($model) {
+            $model->addFillable([
+                'bio'
+            ]);
+        });
+
         UsersController::extendFormFields(function($form, $model, $context){
             $form->addTabFields([
                 'bio' => [
