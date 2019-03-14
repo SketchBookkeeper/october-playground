fd5f2e0ce3a476280bdd7e16dd9e45a773110a9d
Paul Allen <p.allen.art@gmail.com>
2019-03-14 14:38:23 -0600

    Create movie filter method.

--- plugins/paulallen/movies/components/FilterMovies.php
+++ plugins/paulallen/movies/components/FilterMovies.php
@@ -30,13 +30,40 @@ class FilterMovies extends ComponentBase
 
     public function filterMovies()
     {
-        $request = Request::post();
-        $movies = null;
+        $genres = Input::get('genres');
+        $year   = Input::get('year');
 
-        if (isset($request['genres'])) {
-            // $movie_id = DB::select( 'select movie_id from paulallen_movies_genre_movie where genre_id in ()' );
-            $movies = new Movie();
-            $test = $movies->belongsToGenres();
-        }
+        // Conditional Query
+        //
+        // Build a SQL query based on the Request,
+        // when() will add parts of the query if the first
+        // argument is met.
+        //
+        // https://laravel.com/docs/5.5/queries#conditional-clauses
+
+        $movies = Movie::when($genres, function($query, $genres) {
+            // Pass the variable $genres with variable inheriting to the closure (it's like a callback).
+            // https://stackoverflow.com/questions/34896236/laravel-where-has-passing-additional-arguments-to-function
+
+            $query->whereHas('genres', function($query) use ($genres) {
+                // whereHas() allows us to join the tables where genre and movie pivot.
+                // Then we do a IN compare against the genre ids provided
+                // https://laravel.io/forum/04-07-2014-search-post-by-many-tags
+                //
+                // It all comes out to look like this...
+                //
+                // select * from `paulallen_movies_` where exists
+                // (select * from `paulallen_movies_genres_` inner join `paulallen_movies_genre_movie`
+                // on `paulallen_movies_genres_`.`id` =
+                // `paulallen_movies_genre_movie`.`genre_id` where `paulallen_movies_`.`id` =
+                // `paulallen_movies_genre_movie`.`movie_id` and `genre_id` in ('1', '2'))
+
+                $query->whereIn('genre_id', explode(',',$genres));
+            });
+        })
+        ->when($year, function($query, $year) {
+            $query->where('year', $year);
+        })
+        ->get();
     }
 }
